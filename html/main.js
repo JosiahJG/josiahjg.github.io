d8a1851ba35ae104f46349e41cfaa1b520a58680
Josiah Jacobsen-Grocott <josiah.jg@gmail.com>
2020-08-01 22:53:09 -0500

    Set light or dark banners

--- html/main.js
+++ html/main.js
@@ -1,4 +1,4 @@
-var lightbgs = {0:true,3:true,6:true,8:true};
+var lightbgs = {3:true,8:true};
 
 var r;
 if(!localStorage.getItem('bgNum') || localStorage.getItem('bgTime') < (new Date()).getTime()- 1000*60*60*12){
@@ -8,10 +8,8 @@ if(!localStorage.getItem('bgNum') || localStorage.getItem('bgTime') < (new Date(
 } else {
   r = localStorage.getItem('bgNum');
 }
-r=2;
-console.log(["debug", banner]);
 
 document.body.style["background-image"] =  'url("images/background'+r+'.jpg")';
 var banner = document.getElementById("banner");
-if(lightbgs[r]) banner.className = "lightbg";
-else banner.className = "darkbg";
+if(lightbgs[r]) banner.style.color = "#000";
+else banner.style.color = "#fff";
